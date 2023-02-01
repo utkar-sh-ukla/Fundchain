10836eecc89e3e30b2f435330710dfc8714bccdd
utkar-sh-ukla <utkarshshukla707@gmail.com>
2023-02-02 00:32:55 +0530

    web3 context

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -2,12 +2,15 @@ import React, {useState} from 'react'
 import {useNavigate} from 'react-router-dom';
 import {ethers} from 'ethers';
 
+import {useStateContext} from '../context';
 import {money} from '../assets';
 import {CustomButton, FormField, Loader} from '../components';
+import {checkIfImage} from '../utils';
 
 const CreateCampaign = () => {
     const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(false);
+    const {createCampaign} = useStateContext();
     const [form, setForm] = useState({
         name: '',
         title: '',
@@ -24,7 +27,17 @@ const CreateCampaign = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(form)
+        checkIfImage(form.image, async (exists) => {
+            if (exists) {
+                setIsLoading(true)
+                await createCampaign({...form, target: ethers.utils.parseUnits(form.target, 18)})
+                setIsLoading(false);
+                navigate('/');
+            } else {
+                alert('Provide valid image URL')
+                setForm({...form, image: ''});
+            }
+        })
     }
 
     return (
